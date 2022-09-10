9a9172bae9a4de9f96e612a9c1cbef357cf871a7
HoangLinh51 <linhle5112002@gmail.com>
2022-09-10 19:52:19 +0700

    video6

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+
+const app = express();
+const port = 2002
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('./index.ejs')
+})
+app.get('/about', (req, res) => {
+    res.send(`I'm Newbie!`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
