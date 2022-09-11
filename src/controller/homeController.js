e1089ac2a68e8ec14ef31cec461592c00dabc0f8
HoangLinh51 <linhle5112002@gmail.com>
2022-09-11 20:47:04 +0700

    video8

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,6 @@
 let getHomepage = (req, res) => {
-    return res.render('/index.ejs')
+    return res.render('./index.ejs')
 }
-//logic
 
 module.exports = {
     getHomepage
--- src/route/web.js
+++ src/route/web.js
@@ -5,11 +5,8 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/about', (req, res) => {
-        res.send(`I'm Linh!`)
+        res.send(`I'm Eric!`)
     })
-
     return app.use('/', router)
 }
-
-
 export default initWebRoute;
